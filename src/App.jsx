ee9109db4dc105b3ff89816f2157f7a1d8f8aa4d
Moises Godinho <moisesgodinho0067@gmail.com>
2024-05-29 21:55:06 -0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { FaReact } from "react-icons/fa";
+
+const App = () => {
+  return (
+    <>
+      <h1 className="text-7xl font-bold text-blue-300">Hello</h1>
+      <FaReact size={50} className="text-red-400" />
+    </>
+  );
+};
+
+export default App;
